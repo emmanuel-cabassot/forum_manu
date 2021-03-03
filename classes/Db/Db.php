5844ae66da8533f33c8a9d5cf3931041fa017940
emmanuel cabassot <emmanuel.cabassot@laplateforme.io>
2021-03-03 12:32:13 +0100

    Forum

---
+++ classes/Db/Db.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\classes\Db;
+
+// On "importe" PDO
+use PDO;
+use PDOException;
+
+class Db extends PDO
+{
+    // Instance unique de la classe
+    private static $instance;
+
+    // Informations de connexion
+    private const DBHOST = 'localhost';
+    private const DBUSER = 'root';
+    private const DBPASS = '';
+    private const DBNAME = 'forum';
+
+    private function __construct()
+    {
+        // DSN de connexion
+        $_dsn = 'mysql:dbname='. self::DBNAME . '; charset=utf8; host=' . self::DBHOST;
+
+        // On appelle le constructeur de la classe PDO
+        try{
+            parent::__construct($_dsn, self::DBUSER, self::DBPASS);
+
+            $this->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+            $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }catch(PDOException $e){
+            die($e->getMessage());
+        }
+    }
+
+    public static function getInstance():self
+    {
+        if(self::$instance === null){
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+}
